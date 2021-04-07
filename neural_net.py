fa22d90784e9ed6840f3d652f33480ee50278619
Jose Hernandez <jhernandez.sld@e.email>
2021-04-07 02:23:57 -0500

    updated and cleaned coda a bit

--- neural_net.py
+++ neural_net.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
 import numpy as np
-from typing import List, Callable, Any, Dict, TypeVar, Union
+from typing import List, Callable, Any, Dict, TypeVar, Union, Tuple
 from functools import reduce
-import numpy.random as rand
-from numpy.lib.function_base import vectorize
+
 
 T = TypeVar("T")
 
@@ -51,30 +50,10 @@ def matrix_map_partial(map_dict: Dict[str, float]) -> Callable[[str], float]:
         return map_dict[cell_type]
     return f
 
-
-vmap = np.vectorize(matrix_map_partial({"n": 0.995, "g": 0.92, "b": 0.002}))
-modelos_numericos = vmap(read_matrix_from_file("number_model.csv", " "))
-numero_de_filas = 5
-numero_de_columnas = 3
-tamanio = numero_de_filas * numero_de_columnas
-ciclos_de_entrenamiento = 10_000
-tasa_original = 0.15
-tranqui = 0.999999998
-ultimo_digito = 9
-digitos = [x for x in range(ultimo_digito)]
-profundidad_binario = len(to_binary(ultimo_digito + 1))
-
-weights = np.matrix(
-    np.split(
-        np.array(rand.uniform(size=tamanio * profundidad_binario)),
-        profundidad_binario,
-    )
-)
-
-tasa = tasa_original
-entradas_entrenamiento = rand.randint(0, ultimo_digito, ciclos_de_entrenamiento)
-print(weights)
-
+def compose_functions(f: Callable, g: Callable) -> Callable:
+    def fog(x:Any) -> Any:
+        return g(f(x))
+    return fog
 def sigmoid(x: Union[int,float]) -> float:
     return 1 / (1 + np.exp(-x))
 
@@ -83,55 +62,76 @@ def sigmoid_derivative(x: Union[int,float])-> float:
 
 def bool_to_float(x: bool)->float:
     return float(x)
-    
+     
 def grather_than_zero(x: float)->bool:
     return x >= 0
 
-def neural_net():
-    def train(weights: np.matrixlib.matrix, train_input: List[int], gradient: Callable[[Any], float], activation: Callable[[Any], float]):
-        def _train(case:int, weights: np.matrixlib.matrix, gradient: Callable[[Any], float], activation: Callable[[Any], float]):
-            pass
-        [_train(case_input, weights, gradient, activation) for case_input in train_input]
-        pass
-    
-    return trained_model
+def crear_digito_con_modelo(ultimo_digito: int, ciclos: int,tamanio: int, modelos_numericos: np.matrix)->List[Tuple[int, np.matrix]]:
+    def _digito_a_modelo(digito:int):
+        return  (np.random.uniform(0, 1, tamanio) < modelos_numericos[digito]).astype(int)
+    return [(digito, _digito_a_modelo(digito)) for digito in np.random.randint(0, ultimo_digito, ciclos)]
+# def nnural_net():
+#     def train(train_input: List[int], gradient: Callable[[Any], float], activation: Callable[[Any], float]):
+#         def setup_train(case:int, gradient: Callable[[Any], float], activation: Callable[[Any], float]):
+#             def _train(weights: np.matrix) -> np.matrix:
+#                 pass
+#             return _train
+#         return reduce(compose_functions, (setup_train(case_input, gradient, activation) for case_input in train_input))
+#     return train
+
+vmap = np.vectorize(matrix_map_partial({"n": 0.995, "g": 0.92, "b": 0.002}))
+modelos_numericos = vmap(read_matrix_from_file("number_model.csv", " "))
+numero_de_filas = 5
+numero_de_columnas = 3
+tamanio = numero_de_filas * numero_de_columnas
+ciclos_de_entrenamiento = 15_000
+ultimo_digito = 9
+digitos = [x for x in range(ultimo_digito)]
+profundidad_binario = len(to_binary(ultimo_digito))
+
+weights = np.matrix(
+    np.split(
+        np.array(np.random.uniform(size=tamanio * profundidad_binario)),
+        profundidad_binario,
+    )
+)
 
 
-for train in range(ciclos_de_entrenamiento):
-    numero_prueba = entradas_entrenamiento[train]
-    modelo_entrada_entrenamiento = rand.uniform(0, 1, tamanio) < modelos_numericos[numero_prueba]
-    valores_binarios_esperados = to_binary_fixed(numero_prueba, profundidad_binario)
-    entrada = np.full((profundidad_binario, tamanio), modelo_entrada_entrenamiento)
-    new_weights = np.dot(weights, entrada.T)
-    
-    #nw = []
-    #for valor_esperado_profundidad, weight in zip(valores_binarios_esperados, weights):
-    #    sum_pesos = float(np.dot(weight, modelo_entrada_entrenamiento.T))
-    #    output = bool(0 <= sum_pesos) # activation fn
-    #    if output != valor_esperado_profundidad:
-    #        ajuste = tasa * (valor_esperado_profundidad - output)
-    #        tasa = tranqui * tasa
-    #        nw.append(weight + (ajuste * modelo_entrada_entrenamiento))
-    #    else:
-    #        nw.append(weight)
+entradas_entrenamiento = crear_digito_con_modelo(ultimo_digito, ciclos_de_entrenamiento, tamanio, modelos_numericos)
+entradas_prueba = np.random.randint(0, ultimo_digito, 5000)
+valor_aceptacion = 0.9
+output_function = lambda x: sigmoid(x)
+fix_function = lambda x: bool(x >= 0.1)
+gradient_function = lambda x: max(0, x) # sigmoid_derivative(x)
+
+for salida, modelo_entrada in entradas_entrenamiento:
+    valores_binarios_esperados = to_binary_fixed(salida, profundidad_binario)
+    entrada = np.full((profundidad_binario, tamanio), modelo_entrada)
+    #new_weights = np.dot(weights, entrada.T)
+    nw = []
+    for valor_esperado_profundidad, weight in zip(valores_binarios_esperados, weights):
+        sum_pesos = float(np.dot(weight, modelo_entrada.T))
+        output = output_function(sum_pesos) #  activation fn
+        error = valor_esperado_profundidad - output
+        ajuste = gradient_function(output) *  error 
+        nw.append(weight + (ajuste * modelo_entrada))
         
 
-    #weights = np.array(nw)
+    weights = np.array(nw)
 
 #test
-entradas_prueba = rand.randint(0, ultimo_digito, 1000)
 print(f"{weights}")
-print("t")
 res = []
 for valor_prueba in entradas_prueba:
-    modelo_entrada_entrenamiento = rand.uniform(0, 1, tamanio) < modelos_numericos[valor_prueba]
+    modelo_entrada_entrenamiento = (np.random.uniform(0, 1, tamanio) < modelos_numericos[valor_prueba]).astype(int)
     valor_esperado_prueba = to_binary_fixed(valor_prueba, profundidad_binario)
-    output = []
+    output_list = []
     for valor_esperado_profundidad, weight in zip(valor_esperado_prueba, weights):
         sum_pesos = float(np.dot(weight, modelo_entrada_entrenamiento.T))
-        output.append(0>=sum_pesos)
-    res.append((to_decimal(output), valor_prueba, to_decimal(output) == valor_prueba))
+        output = fix_function(output_function(sum_pesos))
+        output_list.append(output)
+    res.append((output_list, to_decimal(output_list), valor_prueba, to_decimal(output_list) == valor_prueba))
 # print(f"{res}\n")
-percentaje = np.mean([int(ac)for _,_, ac in res])
-print(tasa)
+percentaje = np.mean([int(ac)for _,_,_, ac in res])
+#print(res)
 print(percentaje)
