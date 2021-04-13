d9e18e26d823ef02dedce6b33c59da6804b75bdb
Jose Hernandez <jhernandez.sld@e.email>
2021-04-12 21:02:02 -0500

    Added changes to generalize code

--- neural_net.py
+++ neural_net.py
@@ -54,11 +54,12 @@ def compose_functions(f: Callable, g: Callable) -> Callable:
     def fog(x:Any) -> Any:
         return g(f(x))
     return fog
+
 def sigmoid(x: Union[int,float]) -> float:
     return 1 / (1 + np.exp(-x))
 
 def sigmoid_derivative(x: Union[int,float])-> float:
-        return x * (1.0 - x)
+        return np.multiply(x, (1 - x))
 
 def bool_to_float(x: bool)->float:
     return float(x)
@@ -66,10 +67,11 @@ def bool_to_float(x: bool)->float:
 def grather_than_zero(x: float)->bool:
     return x >= 0
 
-def crear_digito_con_modelo(ultimo_digito: int, ciclos: int,tamanio: int, modelos_numericos: np.matrix)->List[Tuple[int, np.matrix]]:
-    def _digito_a_modelo(digito:int):
-        return  (np.random.uniform(0, 1, tamanio) < modelos_numericos[digito]).astype(int)
-    return [(digito, _digito_a_modelo(digito)) for digito in np.random.randint(0, ultimo_digito, ciclos)]
+def crear_digito_con_modelo(digito: int, tamanio: int, modelos_numericos: np.matrix)->List[Tuple[int, np.matrix]]:
+    return  (np.random.uniform(0, 1, tamanio) < modelos_numericos[digito]).astype(int)
+
+def forward_propagation(input_value: np.matrix, weights:np.matrix):
+    return np.dot(weights, input_value.T)
 # def nnural_net():
 #     def train(train_input: List[int], gradient: Callable[[Any], float], activation: Callable[[Any], float]):
 #         def setup_train(case:int, gradient: Callable[[Any], float], activation: Callable[[Any], float]):
@@ -84,43 +86,58 @@ modelos_numericos = vmap(read_matrix_from_file("number_model.csv", " "))
 numero_de_filas = 5
 numero_de_columnas = 3
 tamanio = numero_de_filas * numero_de_columnas
-ciclos_de_entrenamiento = 15_000
+ciclos_de_entrenamiento = 1_000#1_000_000_000
 ultimo_digito = 9
 digitos = [x for x in range(ultimo_digito)]
 profundidad_binario = len(to_binary(ultimo_digito))
-
 weights = np.matrix(
     np.split(
         np.array(np.random.uniform(size=tamanio * profundidad_binario)),
         profundidad_binario,
     )
 )
-
-
-entradas_entrenamiento = crear_digito_con_modelo(ultimo_digito, ciclos_de_entrenamiento, tamanio, modelos_numericos)
-entradas_prueba = np.random.randint(0, ultimo_digito, 5000)
+bias = np.zeros(profundidad_binario)
+entradas_entrenamiento = np.random.randint(0, ultimo_digito, ciclos_de_entrenamiento)
+entradas_prueba = np.random.randint(0, ultimo_digito, 100)
 valor_aceptacion = 0.9
 output_function = lambda x: sigmoid(x)
-fix_function = lambda x: bool(x >= 0.1)
-gradient_function = lambda x: max(0, x) # sigmoid_derivative(x)
+fix_function = lambda x:  np.greater(x, valor_aceptacion)
+gradient_function = np.vectorize(lambda x: sigmoid_derivative(x))
 
-for salida, modelo_entrada in entradas_entrenamiento:
+print("s")
+print(weights)
+i=0
+for salida in entradas_entrenamiento:
+    modelo_entrada = crear_digito_con_modelo(salida, tamanio, modelos_numericos)
     valores_binarios_esperados = to_binary_fixed(salida, profundidad_binario)
-    entrada = np.full((profundidad_binario, tamanio), modelo_entrada)
-    #new_weights = np.dot(weights, entrada.T)
-    nw = []
-    for valor_esperado_profundidad, weight in zip(valores_binarios_esperados, weights):
-        sum_pesos = float(np.dot(weight, modelo_entrada.T))
-        output = output_function(sum_pesos) #  activation fn
-        error = valor_esperado_profundidad - output
-        ajuste = gradient_function(output) *  error 
-        nw.append(weight + (ajuste * modelo_entrada))
+    L1 = np.dot(weights, modelo_entrada.T)
+    #for valor_esperado_profundidad, weight in zip(valores_binarios_esperados, weights):
+    g = np.add(L1, bias[np.newaxis,:].T)
+    output = output_function(g).T #  activation fn
+    print(f"output: {output}")
+    error = np.squeeze(valores_binarios_esperados - output)
+    ajuste = np.multiply(gradient_function(output).T, error.T)
+    print(f"ajuste: {ajuste}")
+    rest = 0.25 * (np.multiply(modelo_entrada, ajuste))
+    weights = weights - rest
+    print(f"bias: {bias}, L1:{L1}, error: {error}")
+    bias = bias - 2 * np.multiply(sigmoid_derivative(np.squeeze(sigmoid(L1))), error) # np.squeeze(ajuste)
+    i+=1
+    if i % 10000 == 0:
+        print(f"i: {i}")
+        print(g)
+        print(f"output:{output}, error:{error}, cost: {np.sum(np.square(error))}")
+        print(f"predition:{to_decimal(np.squeeze(fix_function(np.array(output))))}, real: {salida}")
+        print(f"bias: {bias}")
+        print(f"ajuste: {ajuste}")
+        print(f"weights: {weights}")
+        #print(f"ajuste_f1: {np.multiply(ajuste, modelo_entrada)}")
+        #print(f"ajuste_f2: {np.dot(ajuste, modelo_entrada)}")
         
 
-    weights = np.array(nw)
 
 #test
-print(f"{weights}")
+#print(weights)
 res = []
 for valor_prueba in entradas_prueba:
     modelo_entrada_entrenamiento = (np.random.uniform(0, 1, tamanio) < modelos_numericos[valor_prueba]).astype(int)
@@ -133,5 +150,5 @@ for valor_prueba in entradas_prueba:
     res.append((output_list, to_decimal(output_list), valor_prueba, to_decimal(output_list) == valor_prueba))
 # print(f"{res}\n")
 percentaje = np.mean([int(ac)for _,_,_, ac in res])
-#print(res)
+print(res)
 print(percentaje)
--- xor_net.py
+++ xor_net.py
@@ -1,107 +1,203 @@
 #!/usr/bin/env python3
 
 import numpy as np
-from typing import List, Tuple, Callable
+from typing import List, NamedTuple, Tuple, Callable, Any, Dict
+from functools import reduce
+from collections import namedtuple
+
 ActivationFunction = Callable[[np.array], np.array]
 Weights = np.array
 Bias = np.array
 NeuronLayer = Tuple[Weights, Bias, ActivationFunction]
+LayerResult = namedtuple("LayerResult", ["Z", "A"])
+
+
 def sigmoid(z):
-    return 1/(1 + np.exp(-z))
+    return 1 / (1 + np.exp(-z))
 
-def create_neuron_layers(neurons_per_layer: List[int],
-                         activation_functions: List[ActivationFunction]) \
-    -> List[NeuronLayer]:
-    return [(np.random.randn(o,i),np.zeros((o, 1)), f)for i,o,f in zip(neurons_per_layer[:-1], neurons_per_layer[1:], activation_functions)]
-    
 
-def forward_prop(inputs: np.array, neuron_layer_list: List[NeuronLayer]) -> List[np.array]:
-    p = inputs
-    l= []
-    for weights, bias, activation_function in neuron_layer_list:
-        z = np.dot(weights, p) + bias
-        p = activation_function(z)
-        l.append(p)
-    return l
-    # return [activation_function(np.dot(weights, inputs) + bias) for weights, bias, activation_function in neuron_layer_list]
+def compose_functions(f: Callable, g: Callable) -> Callable:
+    def fog(x: Any) -> Any:
+        return g(f(x))
 
-def calculate_cost(An, Y):
-    cost = -np.sum(np.multiply(Y, np.log(An)) +  np.multiply(1-Y, np.log(1-An)))/m
-    cost = np.squeeze(cost)
+    return fog
 
-    return cost
 
-def backward_prop(inputs: np.array, outputs: np.array,
-                  layer_result_list: List[np.array],
-                  neuron_layer_list: List[NeuronLayer],
-                  m: int) -> List[Tuple[np.array, np.array, np.array]]:
-    def _backward_prop(dZn: np.array, layer_weights_n: Weights,  layer_result: np.array) \
-        -> Tuple[np.array, np.array, np.array]:
-        dZl = np.multiply(np.dot(layer_weights_n.T, dZn), 1 - np.power(layer_result, 2))
-        dWl = np.dot(dZl, layer_result.T) / m
-        dbl = np.sum(dZl, axis=1, keepdims=True)/m
+def print_var(**varsd):
+    return print(", ".join(f"{k}: {v}" for k, v in varsd.items()))
+
+
+def create_neuron_layers(
+    neurons_per_layer: List[int], activation_functions: List[ActivationFunction]
+) -> List[NeuronLayer]:
+    return [
+        (np.random.randn(o, i), np.zeros((o, 1)), f)
+        for i, o, f in zip(
+            neurons_per_layer[:-1], neurons_per_layer[1:], activation_functions
+        )
+    ]
+
+
+def forward_propagation(
+    inputs: np.array, neuron_layer_list: List[NeuronLayer]
+) -> List[LayerResult]:
+    def partial_forward_propagation(weights, bias, activation_fn):
+        def _forward_propagation(A_Z_list):
+            Z = np.dot(weights, A_Z_list[-1].A) + bias
+            return A_Z_list + [LayerResult(Z, np.squeeze(activation_fn(Z)))]
+
+        return _forward_propagation
+
+    partial_forward_propagation_list = [
+        partial_forward_propagation(w, b, f) for w, b, f in neuron_layer_list
+    ]
+    recursive_forward_propagation = reduce(
+        compose_functions, partial_forward_propagation_list
+    )
+    return recursive_forward_propagation([LayerResult(0, inputs)])
+
+
+def cross_entropy_cost(
+    An: np.array, outputs: np.array, number_of_training_examples: int
+):
+    return np.squeeze(
+        -np.sum(
+            np.multiply(outputs, np.log(An)) + np.multiply(1 - outputs, np.log(1 - An))
+        )
+        / number_of_training_examples
+    )
+
+
+def sqr_error_cost(An: np.array, outputs: np.array, number_of_training_examples: int):
+    return np.sum(np.square(An - outputs)) / number_of_training_examples
+
+
+def sqr_error_cost_derivative(An, outputs):
+    return 2 * (An - outputs)
+
+
+def backward_prop(
+    inputs: np.array,
+    outputs: np.array,
+    A_Z_list: List[LayerResult],
+    neuron_layer_list: List[NeuronLayer],
+    number_of_training_examples: int,
+    derivatives: Dict[str, Callable[[np.array], np.array]],
+    cost_derivative_function: Callable[[np.array, np.array, int], float],
+) -> List[Tuple[np.array, np.array, np.array]]:
+    def _backward_prop(
+        dZn: np.array,
+        layer_weights_n: Weights,
+        layer_result: np.array,
+        Z: np.array,
+        derivative_function: Callable[[np.array], np.array],
+    ) -> Tuple[np.array, np.array, np.array]:
+        print_var(a=layer_weights_n)
+        dZl = np.multiply(
+            np.dot(layer_weights_n, dZn), derivative_function(layer_result)
+        )
+        dWl = np.dot(layer_result, dZl.T) / number_of_training_examples
+        dbl = np.sum(dZl, axis=1, keepdims=True) / number_of_training_examples
         return (dZl, dWl, dbl)
-        
-    dZn = layer_result_list[-1] - outputs
-    dbn = np.sum(dZn, axis=1, keepdims=True)/m
-    dWn = np.dot(dZn, layer_result_list[-2].T)
-    _layer_result_list = [inputs] + layer_result_list
-    h = list(zip(neuron_layer_list[1:],_layer_result_list[::-1][2:]))
-    print(h)
-    partial_gradient_list = [_backward_prop(dZn, w, A) for (w, _, _), A in h]
-    return partial_gradient_list + [(dZn, dWn, dbn)]    
-    
+
+    dAn = cost_derivative_function(A_Z_list[-1].A, outputs, number_of_training_examples)
+    dZn = derivatives[neuron_layer_list[-1][2].__name__](A_Z_list[-1].A) * dAn
+    dWn = np.dot(dZn, A_Z_list[-2].A.T)
+    dbn = np.sum(dZn, axis=0, keepdims=True)
+    print_var(dAn=dAn, dZn=dZn, dWn=dWn, dbn=dbn)
+    _A_Z_list = [0, inputs] + A_Z_list
+    h = list(zip(neuron_layer_list[1:], _A_Z_list[::-1][2:]))
+    partial_gradient_list = [
+        _backward_prop(dZn, w, lr.A, lr.Z, derivatives[f.__name__])
+        for (w, _, f), lr in h
+    ]
+    return partial_gradient_list + [(dZn, dWn, dbn)]
+
 
 def update_parameters(neuron_layer_list: List[NeuronLayer], grads, learning_rate):
-    return [(w - learning_rate*dW, b - learning_rate*db, f)
-            for (w,b,f), (_, dW, db) in list(zip(neuron_layer_list, grads))]
-        
-def model(inputs: np.array, outputs: np.array, neurons_per_layer: List[int],
-          activation_functions: List[ActivationFunction], num_of_iters: int,
-          learning_rate: float, m: int):
+    return [
+        (w - learning_rate * dW, b - learning_rate * db, f)
+        for (w, b, f), (_, dW, db) in list(zip(neuron_layer_list, grads))
+    ]
+
+
+def model(
+    inputs: np.array,
+    outputs: np.array,
+    neurons_per_layer: List[int],
+    activation_functions: List[ActivationFunction],
+    num_of_iters: int,
+    learning_rate: float,
+    cost_function: Callable[[np.array, np.array, int], float],
+    number_of_trainig_examples: int,
+    derivatives: Dict[str, Callable[[np.array], np.array]],
+):
     neuron_layer_list = create_neuron_layers(neurons_per_layer, activation_functions)
-    for i in range(0, num_of_iters+1):
-        layer_results = forward_prop(inputs, neuron_layer_list)
+    for i in range(0, num_of_iters + 1):
+        layer_results = forward_propagation(inputs, neuron_layer_list)
+        # print(f"lr: {layer_results}")
+        cost = cost_function(layer_results[-1].A, outputs, number_of_trainig_examples)
 
-        cost = calculate_cost(layer_results[-1], outputs)
-
-        grads = backward_prop(inputs, outputs, layer_results, neuron_layer_list, m)
+        grads = backward_prop(
+            inputs,
+            outputs,
+            layer_results,
+            neuron_layer_list,
+            number_of_trainig_examples,
+            derivatives,
+            sqr_error_cost,
+        )
 
         neuron_layer_list = update_parameters(neuron_layer_list, grads, learning_rate)
 
-        if(i%100 == 0):
-            print('Cost after iteration# {:d}: {:f}'.format(i, cost))
+        if i % 100 == 0:
+            # print(f"An:{layer_results[-1]}, o:{outputs}")
+            print("Cost after iteration# {:d}: {:f}".format(i, cost))
 
     return neuron_layer_list
 
+
 def predict(inputs, trained_neuron_layer_list: List[NeuronLayer]):
-    predict_neuron_layer_list = forward_prop(inputs, trained_neuron_layer_list)
-    yhat = predict_neuron_layer_list[-1]
+    predict_neuron_layer_list = forward_propagation(inputs, trained_neuron_layer_list)
+    yhat = predict_neuron_layer_list[-1][1]
     yhat = np.squeeze(yhat)
     return np.greater(yhat, 0.5)
 
-    
+
 np.random.seed(1)
 
-#The 4 training examples by columns
+# The 4 training examples by columns
 inputs = np.array([[0, 0, 1, 1], [0, 1, 0, 1]])
 
-#The outputs of the XOR for every example in X
+# The outputs of the XOR for every example in X
 outputs = np.array([[0, 1, 1, 0]])
 
-#No. of training examples
-m = inputs.shape[1]
+# No. of training examples
+number_of_training_examples = inputs.shape[1]
 
-#Test 2X1 vector to calculate the XOR of its elements. 
-#Try (0, 0), (0, 1), (1, 0), (1, 1)
-X_test = np.array([[1], [1]])
-neurons_per_layer = [2,2,1]
+# Test 2X1 vector to calculate the XOR of its elements.
+# Try (0, 0), (0, 1), (1, 0), (1, 1)
+X_test = np.array([[1, 0, 0, 1], [1, 0, 1, 0]])
+neurons_per_layer = [2, 2, 1]
 activation_functions = [np.tanh, np.tanh, sigmoid]
+derivatives = {
+    np.tanh.__name__: lambda x: 1 - np.power(x, 2),
+    sigmoid.__name__: lambda x: np.multiply(1, (1 - x)),
+}
 num_of_iters = 100000
 learning_rate = 0.3
 
-trained_parameters = model(inputs, outputs, neurons_per_layer, activation_functions,
-                           num_of_iters, learning_rate, m)
+trained_parameters = model(
+    inputs,
+    outputs,
+    neurons_per_layer,
+    activation_functions,
+    num_of_iters,
+    learning_rate,
+    sqr_error_cost,
+    number_of_training_examples,
+    derivatives,
+)
 
 y_predict = predict(X_test, trained_parameters)
 
